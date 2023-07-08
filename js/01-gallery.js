a1a11fb6ca611f84ac139a09e7aa6143d89ac0d4
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-07-08 18:18:05 +0300

    task-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,41 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+const galleryListMarkup = galleryItems
+    .map(({ preview, original, description }) =>
+//  return `<li class="gallery__item">
+        `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`)
+    .join("");
+
+galleryEl.insertAdjacentHTML("beforeend", galleryListMarkup);
+
+galleryEl.addEventListener("click", onImgClick);
+
+function onImgClick(evt) { 
+    evt.preventDefault();
+    if (!evt.target.classList.contains("gallery__image")) {
+        return;
+    } else { 
+        console.log(`Img Clicked`);
+
+        const popOriginalImg = evt.target.dataset.source;
+
+        const instance = basicLightbox.create(`
+    <img src="${popOriginalImg}" width="800" height="600">`)
+
+    instance.show()
+
+    }
+};
